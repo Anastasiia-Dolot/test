453af92e32c9eb87f73c81c8245fac78506e034f
Anastasiia_Dolot <Anastasiia_Dolot@epam.com>
2022-02-16 10:40:38 +0200

    add app

---
+++ db.php
@@ -0,0 +1,11 @@
+<?php
+session_start();
+
+$conn = mysqli_connect(
+  'anastasiia-dolot.database.windows.net',
+  'admin1',
+  'Tg45Sc!d1',
+  'php_mysql_crud'
+) or die(mysqli_error($mysqli));
+
+?>
