7289b2249c5e742a5f8a337a40edfb7412dff0e5
Anastasiia Dolot <83067897+Anastasiia-Dolot@users.noreply.github.com>
2022-02-18 02:07:11 +0200

    Update commands.sh

--- commands.sh
+++ commands.sh
@@ -1,21 +1,20 @@
 #!/bin/bash
 
-sudo su
-apt-get install apache2 -y
-systemctl start apache2
-systemctl enable apache2
-add-apt-repository -y ppa:ondrej/php
-apt-get update
-apt-get install php7.2 php7.2-cli php7.2-common -y
-apt-get install php7.2-curl php7.2-gd php7.2-json php7.2-mbstring php7.2-intl php7.2-mysql php7.2-xml php7.2-zip -y
-a2enmod php7.2
-systemctl restart apache2
+sudo apt-get install apache2 -y
+sudo systemctl start apache2
+sudo systemctl enable apache2
+sudo add-apt-repository -y ppa:ondrej/php
+sudo apt-get update
+sudo apt-get install php7.2 php7.2-cli php7.2-common -y
+sudo apt-get install php7.2-curl php7.2-gd php7.2-json php7.2-mbstring php7.2-intl php7.2-mysql php7.2-xml php7.2-zip -y
+sudo a2enmod php7.2
+sudo systemctl restart apache2
 
-git clone https://github.com/Anastasiia-Dolot/test.git
-cd /var/www/html && rm index.html && cp -r /home/azureuser/test/* .
-touch favicon.ico
-ls -1 | xargs -I{} chmod 777 {} && cd includes && ls -1 | xargs -I{} chmod 777 {}
+sudo git clone https://github.com/Anastasiia-Dolot/test.git
+sudo cd /var/www/html && sudo rm index.html && sudo cp -r /home/azureuser/test/* .
+sudo touch favicon.ico
+ls -1 | xargs -I{} sudo chmod 777 {} && cd includes && ls -1 | xargs -I{} sudo chmod 777 {}
 
-apt install mysql-client-core-8.0
+sudo apt install mysql-client-core-8.0
 
 wget --no-check-certificate https://dl.cacerts.digicert.com/DigiCertGlobalRootCA.crt.pem
