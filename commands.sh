4d455cb4cffaceb6580f8c4468bee63eaf38cf24
Anastasiia Dolot <83067897+Anastasiia-Dolot@users.noreply.github.com>
2022-02-18 13:15:44 +0200

    Update commands.sh

--- commands.sh
+++ commands.sh
@@ -3,14 +3,15 @@
 sudo apt-get install apache2 -y
 sudo systemctl start apache2
 sudo systemctl enable apache2
-sudo add-apt-repository -y ppa:ondrej/php
+sudo add-apt-repository ppa:ondrej/php -y
 sudo apt-get update
 sudo apt-get install php7.2 php7.2-cli php7.2-common -y
 sudo apt-get install php7.2-curl php7.2-gd php7.2-json php7.2-mbstring php7.2-intl php7.2-mysql php7.2-xml php7.2-zip -y
-sudo a2enmod php7.2
 sudo systemctl restart apache2
 
-git clone https://github.com/Anastasiia-Dolot/test.git && sleep 5 && cd /var/www/html && sudo rm index.html && sudo cp -r /home/azureuser/test/* .
+git clone https://github.com/Anastasiia-Dolot/test.git
+sleep 5
+cd /var/www/html && sudo rm index.html && sudo cp -r /home/azureuser/test/* .
 sudo touch favicon.ico
 ls -1 | xargs -I{} sudo chmod 777 {} && cd includes && ls -1 | xargs -I{} sudo chmod 777 {}
 
